3e11a4e2c33074859facd0619740c835bb171e4a
pb1409 <105663678+pb1409@users.noreply.github.com>
2022-06-22 12:21:12 +0100

    Correct all the comments

--- src/pt/ipbeja/po2/chartracer/model/Util.java
+++ src/pt/ipbeja/po2/chartracer/model/Util.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Escola Superior de Tecnologia e Gestão do
+ * Escola Superior de Tecnologia e Gestão
  * Instituto Politécnico de Beja
  * @author José Francisco - 22896, Patrícia Berenguer - 22893
  */
